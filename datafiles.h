f4abb9adc978612c9e6186472df420f92dc29c67
rjanish <ryan.janish@gmail.com>
2023-01-17 11:47:57 -0600

    as above

--- datafiles.h
+++ datafiles.h
@@ -1,3 +1,5 @@
+#ifndef DATAFILES_H
+#define DATAFILES_H
 
 #include <string>
 #include <iostream>
@@ -6,13 +8,25 @@
 
 #include<fmt/format.h>
 
+
 /*
-Read the passed stream, extracting a set number of key-value pairs 
-into the passed map. Pairs are assumed to be listed sequentially 
-and whitespace separated.  
+Linear grid evaluator
+*/
+double linear_step(double start, double end, size_t N, size_t index)
+
+/*
+Log-spaced grid evaluator
+*/
+double log_step(double start, double end, size_t N, size_t index)
+
+
+/*
+Read the passed stream, extracting a set number of key-value pairs
+into the passed map. Pairs are assumed to be listed sequentially
+and whitespace separated.
 */
 template <typename KeyType, typename ValueType>
-void read_param_entries(std::istream & in, 
+void read_param_entries(std::istream & in,
                         std::map<KeyType, ValueType> & params,
                         size_t num_entries)
 {
@@ -20,17 +34,17 @@ void read_param_entries(std::istream & in,
     ValueType value;
     for (size_t counter = 0; counter < num_entries; ++counter){
         in >> name >> value;
-        params[name] = value; 
+        params[name] = value;
     }
 }
 
 
 /*
-Print key-value pairs into a passed output stream.  
-Pairs are printed whitespace separated and one pair to a line. 
+Print key-value pairs into a passed output stream.
+Pairs are printed whitespace separated and one pair to a line.
 */
 template <typename KeyType, typename ValueType>
-std::ostream & write_map(std::ostream & out, 
+std::ostream & write_map(std::ostream & out,
                          std::map<KeyType, ValueType> & map)
 {
     typename std::map<KeyType, ValueType>::iterator it = map.begin();
@@ -40,4 +54,6 @@ std::ostream & write_map(std::ostream & out,
     }
     return out;
 }
+
+#endif
 
