b51c8b5820305e948294dfbf45015f451bdec3fb
rjanish <ryan.janish@gmail.com>
2023-01-17 11:47:35 -0600

    as above

--- sample_overlap.cpp
+++ sample_overlap.cpp
@@ -6,33 +6,11 @@
 
 #include <fmt/format.h>
 #include <gsl/gsl_errno.h>
-#include <gsl/gsl_sf_log.h>
-#include <gsl/gsl_sf_exp.h>
 
 #include "overlap.h"
 #include "effective_current.h"
 #include "cylinder_modes.h"
 #include "datafiles.h"
-
-
-double linear_step(double start, double end, size_t N, size_t index)
-{
-    if (N == 1 && index == 0){
-        return start;
-    } else if (N > 1 and index < N){
-        return start + index*(end - start)/(N - 1);
-    } else {
-        std::cerr << "invalid sample number" << std::endl;
-        abort();
-    }
-}
-
-
-double log_step(double start, double end, size_t N, size_t index)
-{
-    return gsl_sf_exp(
-        linear_step(gsl_sf_log(start), gsl_sf_log(end), N, index));
-}
 
 
 int main(int argc, char* argv[]){
