339f04294fccf2d3f599c1778117968b936b6fe9
rjanish <ryan.janish@gmail.com>
2023-01-24 15:46:25 -0600

    nothing

--- integration.cpp
+++ integration.cpp
@@ -5,7 +5,7 @@
 #include "integration.h"
 
 
-void gsl_integrate(gsl_function * gsl_func , double x_min, double x_max, 
+void gsl_integrate(gsl_function * gsl_func , double x_min, double x_max,
                    double atol, double rtol, gsl_integration_method method,
                    double &result, double &error)
 {
@@ -13,9 +13,9 @@ void gsl_integrate(gsl_function * gsl_func , double x_min, double x_max,
     case romberg: {
         size_t neval;
         const size_t max_workspace_size = 30; // this is GSL's max allowed
-        gsl_integration_romberg_workspace * w_romb = 
+        gsl_integration_romberg_workspace * w_romb =
             gsl_integration_romberg_alloc(max_workspace_size);
-        gsl_integration_romberg(gsl_func, x_min, x_max, atol, rtol, 
+        gsl_integration_romberg(gsl_func, x_min, x_max, atol, rtol,
                                 &result, &neval, w_romb);
         error = GSL_MAX(result*rtol, atol);
         gsl_integration_romberg_free(w_romb);
@@ -25,11 +25,11 @@ void gsl_integrate(gsl_function * gsl_func , double x_min, double x_max,
         size_t max_partitions = pow(10, 6); // magic number
         gsl_integration_workspace * w_qags =
             gsl_integration_workspace_alloc(max_partitions);
-        gsl_integration_qags(gsl_func, x_min, x_max, atol, rtol, 
+        gsl_integration_qags(gsl_func, x_min, x_max, atol, rtol,
                              max_partitions, w_qags, &result, &error);
-        gsl_integration_workspace_free(w_qags);     
+        gsl_integration_workspace_free(w_qags);
         return;
-        }   
+        }
     }
     abort();
 }
--- integration.h
+++ integration.h
@@ -7,9 +7,9 @@
 
 
 /*
-Convert any functor f: double --> double to a gsl_function function 
-object. The gsl_function will be given no extra parameters, these 
-must be set via the functor's attributes. 
+Convert any functor f: double --> double to a gsl_function function
+object. The gsl_function will be given no extra parameters, these
+must be set via the functor's attributes.
 */
 template<typename Func>
 double functor_to_function(double x, void * rawptr)
@@ -29,9 +29,9 @@ gsl_function make_gsl_function_from_functor(Func * f){
 
 /*
 Convert any functor f: (double, double) --> double to a single
-argument functor f_fixed double --> double whose one argument is 
-the old functor's second argument. This just holds a pointer to the 
-original functor and the omitted first arguments as a parameter. 
+argument functor f_fixed double --> double whose one argument is
+the old functor's second argument. This just holds a pointer to the
+original functor and the omitted first arguments as a parameter.
 */
 template<typename Func2D>
 class FixFirstArgument {
@@ -51,16 +51,16 @@ Function to quickly switch gsl integration methods
 */
 enum gsl_integration_method {adaptive_singular, romberg};
 
-void gsl_integrate(gsl_function * , double, double, double, double, 
+void gsl_integrate(gsl_function * , double, double, double, double,
                    gsl_integration_method, double &, double &);
 
 /*
 This is functor which computes the innermost integral of a 2D box integral,
 which is assumed to be over the second argument of the integrand:
-  II(x0) = int_x1_min^x1_max dx1 integrand(x0, x1) 
+  II(x0) = int_x1_min^x1_max dx1 integrand(x0, x1)
 The full integral is given by integrating this over the first argument:
-  int_x0_min^x0_max dx0 int_x1_min^x1_max dx1 integrand(x0, x1) = 
-  int_x0_min^x0_max dx0 II(x0) 
+  int_x0_min^x0_max dx0 int_x1_min^x1_max dx1 integrand(x0, x1) =
+  int_x0_min^x0_max dx0 II(x0)
 The integrand is a functor integrand: (double, double) --> double.
 */
 template<typename Func2D>
@@ -73,21 +73,21 @@ private:
     gsl_integration_method method;
     FixFirstArgument<Func2D> integrand_with_fixed_first_arg;
     gsl_function gsl_integrand;
-public: 
+public:
     InnermostIntegration(Func2D * integrand_init,
                          double x1_min_init, double x1_max_init,
                          double atol_init, double rtol_init,
                          gsl_integration_method method_init) :
-      x1_min(x1_min_init), x1_max(x1_max_init), 
+      x1_min(x1_min_init), x1_max(x1_max_init),
       atol(atol_init), rtol(rtol_init), method(method_init),
       integrand_with_fixed_first_arg(
         FixFirstArgument<Func2D>(integrand_init)),
       gsl_integrand(
-        make_gsl_function_from_functor(&integrand_with_fixed_first_arg)) {}    
+        make_gsl_function_from_functor(&integrand_with_fixed_first_arg)) {}
     double operator () (double x0) {
         integrand_with_fixed_first_arg.x0 = x0;
         double result, error;
-        gsl_integrate(&gsl_integrand , x1_min, x1_max, atol, rtol, 
+        gsl_integrate(&gsl_integrand , x1_min, x1_max, atol, rtol,
                       method, result, error);
         return result;
     }
@@ -96,22 +96,22 @@ public:
 
 /*
 Compute the integral of a functor integrand: (double, double) --> double
-over a 2D box [x0_min, x0_max]x[x1_min, x1_max] where x0 is the first 
+over a 2D box [x0_min, x0_max]x[x1_min, x1_max] where x0 is the first
 argument to integrand and x1 the second.
 */
-template<typename Func2D> 
-void integrate_over_2d_box(Func2D * integrand, 
+template<typename Func2D>
+void integrate_over_2d_box(Func2D * integrand,
                            double x0_min, double x0_max,
                            double x1_min, double x1_max,
                            double atol, double rtol,
                            gsl_integration_method method,
                            double &result, double &error)
-{   
-    InnermostIntegration<Func2D> inner_integral(integrand, x1_min, x1_max, 
+{
+    InnermostIntegration<Func2D> inner_integral(integrand, x1_min, x1_max,
                                                 atol, rtol, method);
-    gsl_function gsl_integrand = 
+    gsl_function gsl_integrand =
         make_gsl_function_from_functor(&inner_integral);
-    gsl_integrate(&gsl_integrand , x0_min, x0_max, atol, rtol, 
+    gsl_integrate(&gsl_integrand , x0_min, x0_max, atol, rtol,
                   method, result, error);
 };
 
